0054d0915e10c2846c76fe5a46749a5c252a2483
Miguel Pardo <migsub77@gmail.com>
2016-11-11 19:39:27 -0800

    most updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import SearchForm from './SearchForm';
-// import TripsDisplay from './TripsDisplay';
+import TripsDisplay from './TripsDisplay';
 
 export default function HomePage() {
   return (
     <div>
       <SearchForm />
-      {/* <TripsDisplay /> */}
+      <TripsDisplay />
     </div>
   )
 }
