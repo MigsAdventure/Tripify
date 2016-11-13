f65022460c7fb84c0b3a1fd54607b23b786b9ba1
Ziya Emanet <ziyaemanet@gmail.com>
2016-11-12 16:39:06 -0800

    firebase.js commit

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ const config = {
   authDomain: 'tripify-a5149.firebaseapp.com',
   databaseURL: 'https://tripify-a5149.firebaseio.com',
   storageBucket: 'tripify-a5149.appspot.com',
-  messagingSenderId: '895071215351'
+  messagingSenderId: '895071215351',
 };
 
 export const firebaseApp = firebase.initializeApp(config);
