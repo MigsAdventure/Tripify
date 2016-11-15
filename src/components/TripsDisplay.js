8eda3ce398b570be3cf392e82ff5a4f49234f5c5
Miguel Pardo <migsub77@gmail.com>
2016-11-14 21:38:19 -0800

    building mytrips comp

--- src/components/TripsDisplay.js
+++ src/components/TripsDisplay.js
@@ -16,12 +16,12 @@ export default class TripsDisplay extends Component {
           results.data.map(trip => {
 
             return (
-            // image needs another call from the backend
-              <div key={trip.id} className='col-xs-6 col-sm-4 col-md-3 col-lg-2' >
-                {trip.photos && <img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${trip.photos[0].photo_reference}&key=AIzaSyCaWpX2l-1xUmYGXzWPTyYONs08LiwHLao` || ''} /> }
+              // image needs another call from the backend
+                <div key={trip.id} className='col-xs-6 col-sm-4 col-md-3 col-lg-2' >
+                  {trip.photos && <img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${trip.photos[0].photo_reference}&key=AIzaSyCaWpX2l-1xUmYGXzWPTyYONs08LiwHLao` || ''} /> }
 
-                <Rating icon='star' size="huge" defaultRating={trip.rating} maxRating={5} />
-                <h4>{trip.name}</h4>
+                  <Rating icon='star' size="huge" defaultRating={trip.rating} maxRating={5} />
+                  <h4>{trip.name}</h4>
               </div>
             );
           })
