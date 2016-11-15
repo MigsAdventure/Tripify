24de4d39fa2f12b22a46c8bb0f1511c294b0149f
Miguel Pardo <migsub77@gmail.com>
2016-11-14 18:26:10 -0800

    images can now be accessed

--- src/components/TripsDisplay.js
+++ src/components/TripsDisplay.js
@@ -14,10 +14,12 @@ export default class TripsDisplay extends Component {
       <div className='container-fluid tripsDisplayContainer'>
         {
           results.data.map(trip => {
+
             return (
             // image needs another call from the backend
               <div key={trip.id} className='col-xs-6 col-sm-4 col-md-3 col-lg-2' >
-                <img src={trip.icon} />
+                {trip.photos && <img src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${trip.photos[0].photo_reference}&key=AIzaSyCaWpX2l-1xUmYGXzWPTyYONs08LiwHLao` || ''} /> }
+
                 <Rating icon='star' size="huge" defaultRating={trip.rating} maxRating={5} />
                 <h4>{trip.name}</h4>
               </div>
