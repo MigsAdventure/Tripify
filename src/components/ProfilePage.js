4fe51201a959130de4a0cfa43e54b7f83f7b6448
Dustin Tomlin <dustin.d.hayes@gmail.com>
2016-11-15 17:26:33 -0800

    brought in data and changed columns

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -3,27 +3,37 @@ import { connect } from 'react-redux';
 import  { browserHistory } from 'react-router';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton'
+import * as FirebaseActions from '../actions/FirebaseActions';
 
-class ProfilePage extends Component {
+
+@connect(state => ({
+  loggedIn: state.auth.authenticated,
+  user: state.auth.user,
+
+}))
+
+export default class ProfilePage extends Component {
   constructor() {
     super();
   }
 
 
   render() {
+    const  { user } = this.props;
+    console.log('user', user);
     return(
       <div >
         <Card id='card' style = {{ textAlign: 'left', width: '100%',height: '100%', flex: 'initial'}}>
           <CardHeader
-            title="Dustin"
-            subtitle="Username: CoolBeans"
-            avatar="http://pre12.deviantart.net/bca1/th/pre/i/2012/146/9/9/harley_quinn_portrait_coloured_by_urbanemma-d516fwr.png"
+            title={user.displayName}
+            subtitle={user.email}
+            avatar={user.photoURL}
           />
           <CardMedia
 
             // overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
           >
-            <img style={{width: '20%', maxWidth: '20%', maxHeight: '20%', minWidth: '20%'}} src="https://www.groupon.com/merchant/wp-content/uploads/2013/07/san-francisco-small-business.jpg" alt="img.jpg" />
+            {/* <img style={{width: '20%', maxWidth: '20%', maxHeight: '20%', minWidth: '20%'}} src="https://www.groupon.com/merchant/wp-content/uploads/2013/07/san-francisco-small-business.jpg" alt="img.jpg" /> */}
           </CardMedia>
           <CardTitle
             title="Settings"
@@ -36,37 +46,37 @@ class ProfilePage extends Component {
             <CardActions>
               <FlatButton
                 style={{height: '65px', textAlign: 'left'}}
-                class='col-md-12'
+                class='col-xs-12 col-sm-12 col-md-12 col-lg-12'
                 id= 'FlatButton' >
-                <i class="material-icons" style={{color: '#966FD6',fontSize: '48px', paddingLeft: '10px', transform: 'translateY(13%)'}}>&#xE55E;</i> Current Trip
+                <i class="material-icons" style={{color: '#966FD6',fontSize: '48px', paddingLeft: '10px', transform: 'translateY(10%)'}}>&#xE55E;</i> Current Trip
               </FlatButton>
 
               <FlatButton
                 style={{height: '65px', textAlign: 'left'}}
-                class='col-md-12'
+                class='col-xs-12 col-sm-12 col-md-12 col-lg-12'
                 id= 'FlatButton'  >
-                <i class="material-icons" style={{color: '#77DD77',fontSize: '48px', paddingLeft: '10px', transform: 'translateY(13%)'}}>mail</i> Email
+                <i class="material-icons" style={{color: '#77DD77',fontSize: '48px', paddingLeft: '10px', transform: 'translateY(10%)'}}>mail</i> Email
               </FlatButton>
 
               <FlatButton
                 style={{height: '65px', textAlign: 'left'}}
-                class='col-md-12'
+                class='col-xs-12 col-sm-12 col-md-12 col-lg-12'
                 id= 'FlatButton' >
-                <i class="material-icons" style={{color: '#FFb347',fontSize: '48px', paddingLeft: '10px', transform: 'translateY(13%)' }}>vpn_key</i> Change Password
+                <i class="material-icons" style={{color: '#FFb347',fontSize: '48px', paddingLeft: '10px', transform: 'translateY(10%)' }}>vpn_key</i> Change Password
               </FlatButton>
 
               <FlatButton
                 style={{height: '65px', textAlign: 'left'}}
-                class='col-md-12'
+                class='col-xs-12 col-sm-12 col-md-12 col-lg-12'
                 id= 'FlatButton'  >
-                <i class="material-icons" style={{color: '#F49AC2', fontSize: '48px', paddingLeft: '10px', transform: 'translateY(13%)'}}>mode_edit</i> Edit Things
+                <i class="material-icons" style={{color: '#F49AC2', fontSize: '48px', paddingLeft: '10px', transform: 'translateY(10%)'}}>mode_edit</i> Edit Things
               </FlatButton>
 
               <FlatButton
                 style={{height: '65px', textAlign: 'left'}}
-                class='col-md-12'
+                class='col-xs-12 col-sm-12 col-md-12 col-lg-12'
                 id= 'FlatButton'  >
-                <i class="material-icons" style={{color:'#FF6961',fontSize: '48px', paddingLeft: '10px', transform: 'translateY(13%)'}}>favorite</i> Favorites
+                <i class="material-icons" style={{color:'#FF6961',fontSize: '48px', paddingLeft: '10px', transform: 'translateY(10%)'}}>favorite</i> Favorites
               </FlatButton>
 
             </CardActions>
@@ -76,4 +86,8 @@ class ProfilePage extends Component {
           )
           }
           }
-          export default ProfilePage;
+
+        ProfilePage.propTypes = {
+
+            user: React.PropTypes.object,
+          };
