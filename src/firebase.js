dcc51b7a86405a504433d815260071def1897dca
Ziya Emanet <ziyaemanet@gmail.com>
2016-11-15 16:01:35 -0800

    use miguel keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,22 @@
 import firebase from 'firebase';
 
 // INIT FIREBASE
-// const config = {
-//   apiKey: 'AIzaSyCaWpX2l-1xUmYGXzWPTyYONs08LiwHLao',
-//   authDomain: 'tripify-a5149.firebaseapp.com',
-//   databaseURL: 'https://tripify-a5149.firebaseio.com',
-//   storageBucket: 'tripify-a5149.appspot.com',
-//   messagingSenderId: '895071215351',
-// };
 const config = {
-  apiKey: 'AIzaSyAiPaxx9QsCHft6xnKSfPOtKgEMXgrgIY0',
-  authDomain: 'test-app-7a5c0.firebaseapp.com',
-  databaseURL: 'https://test-app-7a5c0.firebaseio.com',
-  storageBucket: 'test-app-7a5c0.appspot.com',
-  messagingSenderId: '803419720138',
+  apiKey: 'AIzaSyCaWpX2l-1xUmYGXzWPTyYONs08LiwHLao',
+  authDomain: 'tripify-a5149.firebaseapp.com',
+  databaseURL: 'https://tripify-a5149.firebaseio.com',
+  storageBucket: 'tripify-a5149.appspot.com',
+  messagingSenderId: '895071215351',
 };
+
+// Z's api keys
+// const config = {
+//   apiKey: 'AIzaSyAiPaxx9QsCHft6xnKSfPOtKgEMXgrgIY0',
+//   authDomain: 'test-app-7a5c0.firebaseapp.com',
+//   databaseURL: 'https://test-app-7a5c0.firebaseio.com',
+//   storageBucket: 'test-app-7a5c0.appspot.com',
+//   messagingSenderId: '803419720138',
+// };
 
 
 export const firebaseApp = firebase.initializeApp(config);
