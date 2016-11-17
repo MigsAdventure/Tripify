eabfde3f8b0342f0fc10e183cf7ab1a6c3ed7df1
HONG ZHOU <holly_zhou@yahoo.com>
2016-11-17 00:17:54 -0800

    finish upload image to S3 and store in the mongodb

--- server/models/image.js
+++ server/models/image.js
@@ -5,53 +5,49 @@ const path = require('path');
 const uuid = require('uuid');
 
 const BUCKET_NAME = 'tripify-image-bucket';
-const AWS_URL_BASE = 'https://s3.amazonaws.com/';
+const AWS_URL_BASE = 'https://s3-us-west-1.amazonaws.com';
+
+const mongoose = require('mongoose');
+
+const imageSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  url: { type: String, required: true },
+  Key: { type: String, required: true },
+  createAt: { type: Date, default: Date.now },
+});
+
+imageSchema.statics.upload = function(fileObj, cb) {
+  const { originalname, buffer } = fileObj;
+  const ext = path.extname(originalname);
+  const Key = uuid() + ext;
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key,
+    ACL: 'public-read',
+    Body: buffer,
+  };
+
+  // save data to mongodb
+  s3.putObject(params, (err, result) => {
+    if (err) return cb(err);
+    const url = `${AWS_URL_BASE}/${BUCKET_NAME}/${Key}`;
+    this.create({ name: originalname, url, Key }, cb);
+  });
+};
+
+imageSchema.statics.deleteImage = (id, cb) => {
+  const Key = id;
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key,
+  };
+  s3.deleteObject(params, (err, result) => {
+    if (err) return cb(err);
+    else cb(null, result);
+  });
+};
 
 
-
-
-// const mongoose = require('mongoose');
-//
-//
-// const imageSchema = new mongoose.Schema({
-//   name: {type: String, required: true },
-//   url: {type: String, required: true },
-//   Key: {type: String, required: true },
-//   createAt: {type: Date, default: Date.now }
-// });
-//
-// imageSchema.statics.upload = function(fileObj, cb){
-//   let { originalname, buffer } = fileObj;
-//   let ext = path.extname(originalname);
-//   let Key = uuid() + ext;
-//   let params = {
-//     Bucket: BUCKET_NAME,
-//     Key,
-//     ACL: 'public-read',
-//     Body: buffer
-//   };
-//
-//   // save data to mongodb
-//   s3.putObject(params, (err, result)=>{
-//     if(err) return cb(err);
-//     let url = `${AWS_URL_BASE}/${BUCKET_NAME}/${Key}`;
-//     this.create({ name: originalname, url, Key}, cb);
-//   });
-// };
-//
-// imageSchema.statics.deleteImage = (id, cb) => {
-//   let Key = id;
-//   let params = {
-//     Bucket: BUCKET_NAME,
-//     Key: Key,
-//   };
-//   s3.deleteObject(params, (err, result) => {
-//     if (err) return cb(err);
-//     else cb(null, result);
-//   });
-// };
-//
-//
-// const Image = mongoose.model('Image', imageSchema);
+const Image = mongoose.model('Image', imageSchema);
 
 module.exports = Image;
