32a909650b047a66d9b1ba2652a6949a954281f0
Dustin Tomlin <dustin.d.hayes@gmail.com>
2016-11-18 15:49:55 -0800

    got data in current trips

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,22 @@ import firebase from 'firebase';
 //   messagingSenderId: '895071215351',
 // };
 
-// Z's api keys
+// // Z's api keys
+// const config = {
+//   apiKey: 'AIzaSyAiPaxx9QsCHft6xnKSfPOtKgEMXgrgIY0',
+//   authDomain: 'test-app-7a5c0.firebaseapp.com',
+//   databaseURL: 'https://test-app-7a5c0.firebaseio.com',
+//   storageBucket: 'test-app-7a5c0.appspot.com',
+//   messagingSenderId: '803419720138',
+// };
+
+// Dustin's api keys
 const config = {
-  apiKey: 'AIzaSyAiPaxx9QsCHft6xnKSfPOtKgEMXgrgIY0',
-  authDomain: 'test-app-7a5c0.firebaseapp.com',
-  databaseURL: 'https://test-app-7a5c0.firebaseio.com',
-  storageBucket: 'test-app-7a5c0.appspot.com',
-  messagingSenderId: '803419720138',
+  apiKey: "AIzaSyBhjDmU4Rb3YYaJVsUevi6_IUG7xx9Oa5M",
+  authDomain: "test123-1e63d.firebaseapp.com",
+  databaseURL: "https://test123-1e63d.firebaseio.com",
+  storageBucket: "test123-1e63d.appspot.com",
+  messagingSenderId: "315763032189"
 };
 
 
